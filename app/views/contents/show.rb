f9656ac4ec23ee2083e90572332700184c5f6b94
Joe Sak <joe@joesak.com>
2025-04-07 16:49:51 -0600

    Auto link

--- app/views/contents/show.rb
+++ app/views/contents/show.rb
@@ -5,6 +5,14 @@ class Views::Contents::Show < Views::Base
 
   attr_reader :contents, :content, :title
 
+  def self.url_regex = %r{
+    \b
+    (https?://|www\.)?            # Protocol (http or https) or www
+    ([a-z0-9-]+(\.[a-z]{2,})+)    # Domain name
+    (:[0-9]{1,5})?                # Port (optional)
+    (/[^\s]*)?                    # Path (optional)
+  }ix
+
   def initialize(contents:, content:)
     @contents = contents
     @content = content
@@ -17,7 +25,10 @@ class Views::Contents::Show < Views::Base
       link_to "&larr; back".html_safe, root_path, class: "text-blue-900 hover:underline"
 
       div(class: "mt-4 pt-4 border-t border-gray-300") do
-        content.body.html_safe
+        content.body.gsub(self.class.url_regex) do |url|
+          scheme = url.start_with?("http://", "https://") ? "" : "http://"
+          link_to(url, "#{scheme}#{url}", target: :_blank)
+        end.html_safe
       end
     end
   end
